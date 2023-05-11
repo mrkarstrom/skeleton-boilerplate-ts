6517e462df84924a4a8fa0bf9a69b24b3905dea2
Magnus Karström <magnus@yesweb.se>
2023-05-11 13:09:39 +0200

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
+</script>
+
+<AppShell slotSidebarLeft="w-52 bg-surface-500/10 p-4">
+	<svelte:fragment slot="header"
+		><AppBar regionRowHeadline="mx-auto">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<!-- Find all the Avatar-props here: https://www.skeleton.dev/components/avatars -->
+				<Avatar
+					width="w-10"
+					initials="MK"
+					border="border-4 border-surface-300-600-token hover:!border-primary-500"
+					cursor="cursor-pointer"
+				/>
+			</svelte:fragment>
+			<svelte:fragment slot="headline">(headline)</svelte:fragment>
+		</AppBar></svelte:fragment
+	>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
